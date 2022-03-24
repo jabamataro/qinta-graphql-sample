b2db58b0368493c885961cebb11dd6b0281bf343
Jaba Mataro <jabamataro@gmail.com>
2022-03-24 13:57:15 +0800

    Updated GraphQL query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { map, Observable } from 'rxjs';
+
+interface User {
+  id: string;
+  name: string;
+}
+
+interface Response {
+  user: User[];
+}
+
+const GET_USERS = gql`
+  query getUser {
+    user {
+      id
+      name
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'qinta-graphql-sample';
+
+  users: Observable<User[]>;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.users = this.apollo
+      .watchQuery<Response>({
+        query: GET_USERS,
+      })
+      .valueChanges.pipe(map((result) => result.data.user));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,54 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { ApolloLink, InMemoryCache } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
+
+const uri = 'https://wired-ladybug-81.hasura.app/v1/graphql';
+
+export function createApollo(httpLink: HttpLink) {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      DocumentType: 'application/json',
+    },
+  }));
+
+  const auth = setContext((operation, context) => {
+    return {
+      headers: {
+        Authorization: `MPQb23casSHllAHCFa84n1n3A8qNiUxtlB3AafpFrEmciwzDak4MZ1oFG8mrCaSi`,
+      },
+    };
+  });
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
+  return {
+    link,
+    cache,
+  };
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ApolloModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
